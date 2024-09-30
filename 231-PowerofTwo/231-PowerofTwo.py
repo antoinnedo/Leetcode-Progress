861ea9c2436b7fce75afe08e96d7b306213e0683
Anthony Do <150735443+antoinnedo@users.noreply.github.com>
2024-09-30 10:42:10 -0500

    Update file 9/30/2024, 10:41:58 AM

--- 231-PowerofTwo/231-PowerofTwo.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        '''while n>1:
-            if n%2==0:
-                n=n/2
-            else:
-                return False
-        return n==1 #O(logn), O(1)'''
