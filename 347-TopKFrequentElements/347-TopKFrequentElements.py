02df9741eed7c58798d7ddb0c403ef0b8cd15dbb
Anthony Do <150735443+antoinnedo@users.noreply.github.com>
2024-10-21 12:53:18 -0500

    Update 347-TopKFrequentElements.py

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,12 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        '''
-        create a hashmap {nums:freq}
-        sort the hashmap
-        return the keys
-        '''
-
-        #create a map
+        #create a map O(n), O(n)
         dict = {}
         for i in range(len(nums)):
             if nums[i] in dict:
@@ -14,10 +8,11 @@ class Solution:
             else:
                 dict[nums[i]] = 1
         
-        #sort map
+        #sort map O(n),O(nlogn)
         dict_list = [(keys, values) for keys, values in dict.items()]
         dict_list.sort(key = lambda x: x[1], reverse=True)
 
-        #return key
+        #return key O(n), O(k)
         res = [dict_list[i][0] for i in range(k)]
         return res
+        #space, time: O(n), O(nlogn)
