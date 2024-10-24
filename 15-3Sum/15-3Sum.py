feb47fa3e33c8fe88dc956c1ebd66ac512e4561f
Anthony Do <150735443+antoinnedo@users.noreply.github.com>
2024-10-23 19:57:46 -0500

    Update 15-3Sum.py

---
+++ 15-3Sum/15-3Sum.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        
+        #sort array
+        nums.sort()
+
+        #get num1:
+        for i in range(len(nums)):
+            #skip iteration if it's the same number
+            if nums[i] == nums[i-1] and i>0:
+                continue
+            #two pointer to get num2,3
+            left = i+1
+            right = len(nums)-1
+            target = -nums[i]
+
+            while left < right:
+                cur_sum = nums[left] + nums[right]
+                if cur_sum == target:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left+=1
+                    while left < right and nums[right] == nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+                elif cur_sum > target:
+                    right-=1
+                else:
+                    left+=1
+        return result
