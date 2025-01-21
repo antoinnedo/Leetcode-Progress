c339377f5a6a01ec40b5a6b11eba124820ef8249
Anthony Do <150735443+antoinnedo@users.noreply.github.com>
2025-01-21 00:01:13 -0600

    Update file 1/21/2025, 12:01:10 AM

--- 3289-TheTwoSneakyNumbersofDigitville/3289-TheTwoSneakyNumbersofDigitville.py
+++ 3289-TheTwoSneakyNumbersofDigitville/3289-TheTwoSneakyNumbersofDigitville.py
@@ -1,16 +1,17 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        # Dictionary to track counts of numbers
-        count = {}
-        
-        # Iterate through the nums list to populate the dictionary
+        # Set to track seen numbers
+        seen = set()
+        repeated = []
+
+        # Iterate through nums to find duplicates
         for num in nums:
-            if num in count:
-                count[num] += 1
+            if num in seen:
+                repeated.append(num)
+                # Stop if we found both repeated numbers
+                if len(repeated) == 2:
+                    break
             else:
-                count[num] = 1
-        
-        # Find numbers that appear more than once
-        repeated = [num for num, cnt in count.items() if cnt > 1]
+                seen.add(num)
 
         return repeated
