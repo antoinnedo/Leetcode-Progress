b36b055f744764785c1c4342d99989b7122489c3
Anthony Do <150735443+antoinnedo@users.noreply.github.com>
2025-03-28 18:00:43 -0500

    File updated at 3/28/2025, 6:00:27 PM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
@@ -1,6 +1,11 @@
-# Last updated: 3/28/2025, 5:57:35 PM
+# Last updated: 3/28/2025, 6:00:27 PM
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        '''
+        Explanation: subarray size will increase on the right, 
+        if the sum is bigger than the target then right subarray exclude the left
+        this allows the array to traverse the whole array
+        '''
         nums_sum = 0
         left = 0
         min_length = len(nums) + 1
@@ -13,3 +18,4 @@ class Solution:
                 left += 1
         
         return min_length if min_length <= len(nums) else 0
+
